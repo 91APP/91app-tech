88174243ae719986c02c41151b3657153e9aaec5
alfredotang <asdzxcca0617@gmail.com>
2020-10-19 17:38:24 +0800

    v0.3.1 - 修正活動landing page 手機版導頁問題

--- activity/2020h2-techday/js/main.js
+++ activity/2020h2-techday/js/main.js
@@ -297,8 +297,12 @@ jQuery(document).ready(function($) {
     var navToggler = $('.site-menu-toggle');
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
       e.preventDefault();
-
-      var hash = this.hash;
+	  var hash = this.hash;
+	  
+	  if (hash[0] !== '#') {
+		window.location.href = this.href;
+		return;
+	  }
 
       $('html, body').animate({
         'scrollTop': $(hash).offset().top
